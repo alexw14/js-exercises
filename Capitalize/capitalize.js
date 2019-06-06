eecccc8ce64b3bd37453138d5c705080564418da
Alex Wong <alexw14@gmail.com>
2019-06-06 17:24:15 +0800

    Add Capitalize challenge

---
+++ Capitalize/capitalize.js
@@ -0,0 +1,24 @@
+/**
+ * Capitalize
+ *
+ * Write a function that accepts a string. The function should
+ * capitalize the first letter of each word in the string then
+ * return the capitalized string.
+ *
+ * Examples:
+ * capitalize('a short sentence') === 'A Short Sentence'
+ * capitalize('a lazy fox') === 'A Lazy Fox'
+ * capitalize('look, it is working!') === 'Look, It Is Working!'
+ */
+
+ const capitalize = (string) => {
+  let splitString = string.split(' ');
+  for (let i=0; i<splitString.length; i++) {
+    splitString[i] = splitString[i].charAt(0).toUpperCase() + splitString[i].substring(1);
+  }
+  return splitString.join(' ');
+ }
+
+ console.log(capitalize('a short sentence'));
+ console.log(capitalize('a lazy fox'));
+ console.log(capitalize('look, it is working!'));
