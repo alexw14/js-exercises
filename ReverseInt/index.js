c2ee0ca1fb3e3c683b02b3b57b58e49f6de3de6d
Alex Wong <alexw14@gmail.com>
2019-07-13 16:37:21 -0700

    Add ReverseInt

---
+++ ReverseInt/index.js
@@ -0,0 +1,18 @@
+// --- Directions
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+function reverseInt(n) {
+  let reversed = n.toString().split('').reverse().join('');
+  return parseInt(reversed) * Math.sign(n);
+}
+
+module.exports = reverseInt;
+
+// We can use Math.sign() to replace an if-else statement to check if n is positive or negative.
