a6eaa162e6ea35894140ddb65c861d062ae2a63c
Alex Wong <alexw14@gmail.com>
2019-09-01 11:32:44 -0700

    Add scope notes

--- AdvancedJSConcepts/Foundation.js
+++ AdvancedJSConcepts/Foundation.js
@@ -30,7 +30,7 @@ Global Execution Context creates a Global Object and the 'this' keyword
 /* --- Hoisting --- */
 
 // Example One
-console.log('1 ----------');
+console.log('Hoisting Example 1');
 
 console.log(name);  // undefined
 // variables are partially hoisted. When the code runs, it knows there is going to be a name variable, so it does 
@@ -44,7 +44,7 @@ function print() {
 };
 
 // Example Two (Scope and Hoisting)
-console.log('2 ----------')
+console.log('Hoisting example 2')
 
 var favFood = 'pizza';
 
@@ -56,3 +56,44 @@ var foodThoughts = function () {
 
 console.log(foodThoughts());
 // Because favFood is also defined inside the function, it does not look up to the global environment for favFood.
+
+
+/* --- Function Scope vs Block Scope --- */
+
+console.log('Scope Example');
+// function scope
+if (5 > 4) {
+  var secret = '1234';
+}
+console.log(secret); // print 1234
+// A new scope is created when there is a function
+function secret() {
+  var secret2 = '1234';
+}
+//console.log(secret2); // secret2 is not defined
+
+// with ES6, using let and const keyword will create block scope
+if (5 > 4) {
+  let secret3 = '1234';
+}
+//console.log(secret3); // secret is not defined
+
+console.log('Scope Example 2');
+
+function varLoop() {
+  for (var i = 0; i < 5; i++) {
+    console.log(i);
+  }
+  console.log('final ' + i);
+}
+// varLoop();
+// prints 0,1,2,3,4,final 5
+
+function letLoop() {
+  for (let i = 0; i < 5; i++) {
+    console.log(i);
+  }
+  console.log('final ' + i);
+}
+// letLoop();
+// Error, i is not defined in line 96.
