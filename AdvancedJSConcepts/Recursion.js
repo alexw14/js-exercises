b5e716af6e889766e3cb8c4432f0450ebbab63a1
Alex Wong <alexw14@gmail.com>
2019-09-03 23:32:23 -0700

    Add problems to Recursion.js

--- AdvancedJSConcepts/Recursion.js
+++ AdvancedJSConcepts/Recursion.js
@@ -26,7 +26,7 @@ function collectOdds(arr) {
   helper(arr);
   return result;
 }
-console.log(collectOdds([1,2,3,4,5,6,7,8,9]));
+console.log(collectOdds([1, 2, 3, 4, 5, 6, 7, 8, 9]));
 
 // Pure Recursion
 function collectEvens(arr) {
@@ -40,4 +40,37 @@ function collectEvens(arr) {
   newArr = newArr.concat(collectEvens(arr.slice(1)));
   return newArr;
 }
-console.log(collectEvens([1,2,3,4,5,6,7,8,9]));
+console.log(collectEvens([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
+/*
+Write a function called power which accepts a base and an exponent. The function should return the power of the base to the exponent. The function should mimic the functionality of Math.pow() - do not worry about negative bases and exponents.
+// power(2,0) // 1
+// power(2,2) // 4
+// power(2,4) // 16
+*/
+function power(base, exp) {
+  if (exp === 0) return 1;
+  return base * power(base, exp - 1);
+}
+console.log(power(2, 4));
+
+/*
+Write a function called productOfArray which takes in an array of numbers and returns the product of them all.
+// productOfArray([1,2,3]) // 6
+// productOfArray([1,2,3,10]) // 60
+*/
+function productOfArray(arr) {
+  if (arr.length === 0) return 1;
+  return arr[0] * productOfArray(arr.slice(1));
+}
+
+/*
+Write a function called recursiveRange which accepts a number and adds up all the numbers from 0 to the number passed to the function.
+// recursiveRange(6) // 21
+// recursiveRange(10) // 55
+*/
+function recursiveRange(num) {
+  if (num === 0) return 0;
+  return num + recursiveRange(num - 1);
+}
+
