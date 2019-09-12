ba345e3eb8508fe908a2a9eba38de4db83740405
Alex Wong <alexw14@gmail.com>
2019-09-11 23:46:58 -0700

    add HashTables.js

---
+++ AdvancedJSConcepts/HashTables.js
@@ -0,0 +1,65 @@
+function hash(key, arrayLength) {
+  let total = 0;
+  for (let char of key) {
+    let value = char.charCodeAt(0) - 96;
+    total = (total + value) % arrayLength;
+  }
+  return total;
+}
+
+function improvedHash(key, arrayLength) {
+  let total = 0;
+  let primeNum = 31;
+  for (let i = 0; i < Math.min(key.length, 100); i++) {
+    let char = key[i];
+    let value = char.charCodeAt(0) - 96;
+    total = (total * primeNum + value) % arrayLength;
+  }
+  return total;
+}
+
+// Hash table class example
+
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
+  }
+
+  _hash(key) {
+    let total = 0;
+    let primeNum = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * primeNum + value) % this.keyMap.length;
+    }
+    return total;
+  }
+
+  // Set accepts a key and a value
+  // 1. Hash the key
+  // 2. Store the key-value pair in the hash table array via separate chaining
+  set(key, value) {
+    let index = this._hash(key);
+    if (!this.keyMap[index]) {
+      this.keyMap[index] = [];
+    }
+    this.keyMap[index].push([key, value])
+  }
+
+  // Get accepts a key
+  // 1. Hash the key
+  // 2. Retrieves key-value pair from the hash table
+  // 3. If key is not found, return undefined
+  get(key) {
+    let index = this._hash(key);
+    if (this.keyMap[index]) {
+      for (let i = 0; i < this.keyMap[index].length; i++) {
+        if (this.keyMap[index][i][0] === key) {
+          return this.keyMap[index][i][1];
+        }
+      }
+    }
+    return undefined;
+  }
+}
