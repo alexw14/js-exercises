86d122fb979d7586f8c3bb80146d0ca4a4358c2a
Alex Wong <alexw14@gmail.com>
2020-03-11 22:04:37 -0700

    add funDOM

---
+++ funDOM.js
@@ -0,0 +1,22 @@
+const myImg = document.createElement('img');
+
+myImg.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Los_Angeles_Lakers_logo.svg/291px-Los_Angeles_Lakers_logo.svg.png';
+myImg.style.width = '200px';
+myImg.style.transition = 'all 2s';
+
+document.body.append(myImg);
+
+const sheet = new CSSStyleSheet();
+sheet.replaceSync('* {transition: all 2s}');
+document.adoptedStyleSheets = [sheet];
+
+const allElements = document.body.children;
+
+setInterval(() => {
+  for (let el of allElements) {
+    const rotation = Math.floor(Math.random() * 360);
+    const x = Math.floor(document.body.clientWidth * Math.random());
+    const y = Math.floor(document.body.clientHeight * Math.random());
+    el.style.transform = `translate(${x}px, ${y}px) rotate(${rotation}deg)`;
+  }
+}, 2000);
