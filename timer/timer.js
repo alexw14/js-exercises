178a38221cf4eb8f7768e5aa8f5b2ea02ffb4396
Alex Wong <alexw14@gmail.com>
2020-03-18 21:16:23 -0700

    add style.css, modify app.js and timer.js

--- timer/timer.js
+++ timer/timer.js
@@ -15,10 +15,10 @@ class Timer {
 
   start = () => {
     if (this.onStart) {
-      this.onStart();
+      this.onStart(this.timeRemaining);
     }
     this.tick();
-    this.interval = setInterval(this.tick, 1000);
+    this.interval = setInterval(this.tick, 50);
   }
 
   pause = () => {
@@ -27,9 +27,9 @@ class Timer {
 
   tick = () => {
     if (this.timeRemaining > 0) {
-      this.timeRemaining = this.timeRemaining - 1;
+      this.timeRemaining = this.timeRemaining - 0.05;
       if (this.onTick) {
-        this.onTick();
+        this.onTick(this.timeRemaining);
       }
     } else {
       this.pause();
@@ -45,7 +45,7 @@ class Timer {
   }
 
   set timeRemaining(time) {
-    this.durationInput.value = time;
+    this.durationInput.value = time.toFixed(2);
   }
 
 }
