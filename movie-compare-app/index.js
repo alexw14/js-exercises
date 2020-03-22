d4227db14ab759c97bc4cd4ea8dd437b2fb48b16
Alex Wong <alexw14@gmail.com>
2020-03-21 21:28:29 -0700

    add fetchData and input

--- movie-compare-app/index.js
+++ movie-compare-app/index.js
@@ -1,1 +1,15 @@
-console.log('Hi there!');
+const fetchData = async (searchInput) => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: 'b78408a8',
+      s: searchInput
+    }
+  });
+
+  console.log(response.data);
+};
+
+const input = document.querySelector('#movie-search');
+input.addEventListener('input', (event) => {
+  fetchData(event.target.value);
+});
