c3e2969960bbf5e104fec5f2742bafb4f9c648e6
Alex Wong <alexw14@gmail.com>
2020-03-21 23:02:51 -0700

    refactor html to js file

--- movie-compare-app/index.js
+++ movie-compare-app/index.js
@@ -5,14 +5,39 @@ const fetchData = async (searchInput) => {
       s: searchInput
     }
   });
-
-  console.log(response.data);
+  if (response.data.Error) {
+    return [];
+  }
+  return response.data.Search;
 };
 
-const getMoviesData = event => {
-  fetchData(event.target.value);
+const getMoviesData = async (event) => {
+  const moviesData = await fetchData(event.target.value);
+  dropdown.classList.add('is-active');
+  for (let movie of moviesData) {
+    const option = document.createElement('a');
+    option.classList.add('dropdown-item');
+    option.innerHTML = `
+      <img src="${movie.Poster}" />
+      ${movie.Title}
+    `;
+    resultsWrapper.appendChild(option);
+  }
 };
 
-const movieSearchInput = document.querySelector('#movie-search');
+const root = document.querySelector('.movie-search');
+root.innerHTML = `
+  <label><b>Search For a Movie</b></label>
+  <input class="input" />
+  <div class="dropdown">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results"></div>
+    </div>
+  </div>
+`;
+
+const movieSearchInput = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
 
 movieSearchInput.addEventListener('input', debounce(getMoviesData, 500));
