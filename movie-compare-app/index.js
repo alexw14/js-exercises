52dfbdd7b6dd7eb7c5fb369903d25e8d1041a619
Alex Wong <alexw14@gmail.com>
2020-03-23 20:04:11 -0700

    add renderMovieTemplate and getSelectedMovieData

--- movie-compare-app/index.js
+++ movie-compare-app/index.js
@@ -29,9 +29,63 @@ const getMoviesData = async (event) => {
       <img src="${imgSrc}" />
       ${movie.Title}
     `;
+    option.addEventListener('click', () => {
+      dropdown.classList.remove('is-active');
+      movieSearchInput.value = movie.Title;
+      getSelectedMovieData(movie);
+    });
     resultsWrapper.appendChild(option);
   }
 };
+
+const getSelectedMovieData = async (movie) => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: 'b78408a8',
+      i: movie.imdbID
+    }
+  });
+  document.querySelector('.movie-summary').innerHTML = renderMovieTemplate(response.data);
+}
+
+const renderMovieTemplate = (movieData) => {
+  return `
+    <article class="media">
+      <figure class="media-left">
+        <p class="image">
+          <img src="${movieData.Poster}" />
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <h1>${movieData.Title}</h1>
+          <h4>${movieData.Genre}</h4>
+          <p>${movieData.Plot}</p>
+        </div>
+      </div>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieData.Awards}</p>
+      <p class="subtitle">Awards</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieData.BoxOffice}</p>
+      <p class="subtitle">Box Office</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieData.Metascore}</p>
+      <p class="subtitle">Metascore</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieData.imdbRating}</p>
+      <p class="subtitle">IMDB Rating</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieData.imdbVotes}</p>
+      <p class="subtitle">IMDB Votes</p>
+    </article>
+  `;
+}
 
 const root = document.querySelector('.movie-search');
 root.innerHTML = `
