8eb49718ef66b48c55abbc67b97476ec60ee5309
Alex Wong <alexw14@gmail.com>
2020-03-26 22:21:04 -0700

    update maze generation, add stepThroughCell function

--- canvas-maze-app/app.js
+++ canvas-maze-app/app.js
@@ -41,6 +41,59 @@ const horizontals = Array(cells - 1).fill(null).map(() => {
   return Array(cells).fill(false);
 });
 
-console.log(grid);
-console.log(verticals);
-console.log(horizontals);
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+const shuffle = (arr) => {
+  let counter = arr.length;
+  while (counter > 0) {
+    const index = Math.floor(Math.random() * counter);
+    counter--;
+    const temp = arr[counter];
+    arr[counter] = arr[index];
+    arr[index] = temp;
+  }
+  return arr;
+};
+
+const stepThroughCell = (row, col) => {
+  // If visited cell at [row, col], then return
+  if (grid[row][col]) {
+    return;
+  }
+  // Mark this cell as visited
+  grid[row][col] = true;
+  // Assemble randomly-ordered list of neighbors
+  const neighbors = shuffle([
+    [row - 1, col, 'up'],
+    [row, col + 1, 'right'],
+    [row + 1, col, 'down'],
+    [row, col - 1, 'left']
+  ]);
+  // For each neighbor...
+  for (let neighbor of neighbors) {
+    const [nextRow, nextCol, direction] = neighbor;
+    // See if neighbor is out of bounds
+    if (nextRow < 0 || nextRow >= cells || nextCol < 0 || nextCol >= cells) {
+      continue; // don't break out of the for loop
+    }
+    // If visited neighbor, continue to next neighbor
+    if (grid[nextRow][nextCol]) {
+      continue;
+    }
+    // Remove a wall from either horizontal or vertical
+    if (direction === 'left') {
+      verticals[row][col - 1] = true;
+    } else if (direction === 'right') {
+      verticals[row][col] = true;
+    } else if (direction === 'up') {
+      horizontals[row - 1][col] = true;
+    } else if (direction === 'down') {
+      horizontals[row][col] = true;
+    }
+    // call itself with next row and next column
+    stepThroughCell(nextRow, nextCol);
+  }
+}
+
+stepThroughCell(startRow, startColumn);
