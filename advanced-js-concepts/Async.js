e56bac13c0cbc748a779d35040f2e5a9ffbfdc16
Alex Wong <alexw14@gmail.com>
2020-04-01 22:10:33 -0700

    add notes to async

--- advanced-js-concepts/Async.js
+++ advanced-js-concepts/Async.js
@@ -1,16 +1,16 @@
-const promisify = (item, delay) =>
-  new Promise((resolve) =>
-    setTimeout(() =>
-      resolve(item), delay));
+// First Example
 
-const a = () => promisify('a', 100);
-const b = () => promisify('b', 5000);
-const c = () => promisify('c', 3000);
+const promisify = (item, delay) =>
+  new Promise(resolve => setTimeout(() => resolve(item), delay));
+
+const a = () => promisify("a", 100);
+const b = () => promisify("b", 5000);
+const c = () => promisify("c", 3000);
 
 async function parallel() {
   const promises = [a(), b(), c()];
   const [output1, output2, output3] = await Promise.all(promises);
-  return `parallel is done: ${output1} ${output2} ${output3}`
+  return `parallel is done: ${output1} ${output2} ${output3}`;
 }
 
 async function race() {
@@ -23,9 +23,41 @@ async function sequence() {
   const output1 = await a();
   const output2 = await b();
   const output3 = await c();
-  return `sequence is done ${output1} ${output2} ${output3}`
+  return `sequence is done ${output1} ${output2} ${output3}`;
 }
 
-sequence().then(console.log)
-parallel().then(console.log)
-race().then(console.log)
+sequence().then(console.log);
+parallel().then(console.log);
+race().then(console.log);
+
+// Second Example
+function changeColor(color, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      document.body.style.backgroundColor = color;
+      resolve();
+    }, delay);
+  });
+}
+
+async function changeInSequence() {
+  await changeColor("blue", 1000);
+  await changeColor("red", 1000);
+  await changeColor("yellow", 1000);
+  await changeColor("purple", 1000);
+}
+
+async function changeInParallel() {
+  const c1 = changeColor("blue", 1000);
+  const c2 = changeColor("red", 1000);
+  const c3 = changeColor("yellow", 1000);
+  const c4 = changeColor("purple", 1000);
+  // await c1;
+  // await c2;
+  // await c3;
+  // await c4;
+  const results = await Promise.all([c1, c2, c3, c4]);
+}
+
+changeInSequence();
+// changeInParallel();
