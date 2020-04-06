95a29b1ff95af273d1486940ea1755c605147e2c
Alex Wong <alexw14@gmail.com>
2020-04-05 18:12:16 -0700

    add debounce, chokidar, caporal

--- practice/my-nodemon/index.js
+++ practice/my-nodemon/index.js
@@ -1,3 +1,23 @@
 #!/usr/bin/env node
 
-console.log('nodemon')
+const debounce = require("lodash.debounce");
+const chokidar = require("chokidar");
+const program = require("caporal");
+
+program
+  .version("1.0.0")
+  .argument("[filename]", "Name of a file to execute")
+  .action(args => {
+    console.log(args);
+  });
+
+program.parse(process.argv);
+
+
+// const start = () => {};
+
+// chokidar
+//   .watch(".")
+//   .on("add", () => console.log("added"))
+//   .on("change", () => console.log("changed"))
+//   .on("unlink", () => console.log("unlinked"));
