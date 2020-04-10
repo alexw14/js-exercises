7e026cafd709b15ef55d1ea1167661cceee8b322
Alex Wong <alexw14@gmail.com>
2020-04-09 23:16:23 -0700

    add routes and views directory

--- practice/js-store/index.js
+++ practice/js-store/index.js
@@ -1,75 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
-const usersDB = require("./db/users");
+const authRouter = require("./routes/admin/auth");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ["supersecret"] }));
 
-app.get("/signup", (req, res) => {
-  res.send(`
-    <div>
-      Id is ${req.session.userId}
-      <form method="POST">
-        <input name="email" placeholder="email" />
-        <input name="password" placeholder="password" />
-        <input name="passwordConfirmation" placeholder="password confirmation" />
-        <button>Sign up</button>
-      </form>
-    </div>
-  `);
-});
-
-app.post("/signup", async (req, res) => {
-  const { email, password, passwordConfirmation } = req.body;
-  const existingUser = await usersDB.getOneBy({ email });
-  if (existingUser) {
-    return res.send("Email already taken");
-  }
-  if (password !== passwordConfirmation) {
-    return res.send("Password do not match");
-  }
-  const user = await usersDB.create({ email, password });
-  req.session.userId = user.id;
-
-  res.send("created");
-});
-
-app.get("/signout", (req, res) => {
-  req.session = null;
-  res.send("Logged out");
-});
-
-app.get("/signin", (req, res) => {
-  res.send(`
-    <div>
-      <form method="POST">
-        <input name="email" placeholder="email" />
-        <input name="password" placeholder="password" />
-        <button>Sign In</button>
-      </form>
-    </div>
-  `);
-});
-
-app.post("/signin", async (req, res) => {
-  const { email, password } = req.body;
-  const user = await usersDB.getOneBy({ email });
-  if (!user) {
-    return res.send("Email not found");
-  }
-  const validPassword = await usersDB.comparePasswords(
-    user.password,
-    password
-  );
-  if (!validPassword) {
-    return res.send("Invalid password");
-  }
-  req.session.userId = user.id;
-  res.send("Logged In!");
-});
+app.use(authRouter);
 
 app.listen(3000, () => {
   console.log("Listening");
---
+++ practice/js-store/routes/admin/auth.js
@@ -0,0 +1,51 @@
+const express = require("express");
+
+const usersDB = require("../../db/users");
+const signupTemplate = require("../../views/admin/auth/signup");
+const signinTemplate = require("../../views/admin/auth/signin");
+
+const router = express.Router();
+
+router.get("/signup", (req, res) => {
+  res.send(signupTemplate({ req }));
+});
+
+router.post("/signup", async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body;
+  const existingUser = await usersDB.getOneBy({ email });
+  if (existingUser) {
+    return res.send("Email already taken");
+  }
+  if (password !== passwordConfirmation) {
+    return res.send("Password do not match");
+  }
+  const user = await usersDB.create({ email, password });
+  req.session.userId = user.id;
+
+  res.send("created");
+});
+
+router.get("/signout", (req, res) => {
+  req.session = null;
+  res.send("Logged out");
+});
+
+router.get("/signin", (req, res) => {
+  res.send(signinTemplate());
+});
+
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await usersDB.getOneBy({ email });
+  if (!user) {
+    return res.send("Email not found");
+  }
+  const validPassword = await usersDB.comparePasswords(user.password, password);
+  if (!validPassword) {
+    return res.send("Invalid password");
+  }
+  req.session.userId = user.id;
+  res.send("Logged In!");
+});
+
+module.exports = router;
