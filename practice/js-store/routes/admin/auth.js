d85a91ff7d21287124463b1e34a7460ace65bdf2
Alex Wong <alexw14@gmail.com>
2020-04-09 23:41:48 -0700

    add express-validator

--- practice/js-store/routes/admin/auth.js
+++ practice/js-store/routes/admin/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const { check, validationResult } = require("express-validator");
 const usersDB = require("../../db/users");
 const signupTemplate = require("../../views/admin/auth/signup");
 const signinTemplate = require("../../views/admin/auth/signin");
@@ -10,20 +10,37 @@ router.get("/signup", (req, res) => {
   res.send(signupTemplate({ req }));
 });
 
-router.post("/signup", async (req, res) => {
-  const { email, password, passwordConfirmation } = req.body;
-  const existingUser = await usersDB.getOneBy({ email });
-  if (existingUser) {
-    return res.send("Email already taken");
-  }
-  if (password !== passwordConfirmation) {
-    return res.send("Password do not match");
-  }
-  const user = await usersDB.create({ email, password });
-  req.session.userId = user.id;
+router.post(
+  "/signup",
+  [
+    check("email")
+      .trim()
+      .normalizeEmail()
+      .isEmail(),
+    check("password")
+      .trim()
+      .isLength({ min: 4, max: 20 }),
+    check("passwordConfirmation")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
+    const { email, password, passwordConfirmation } = req.body;
+    const existingUser = await usersDB.getOneBy({ email });
+    if (existingUser) {
+      return res.send("Email already taken");
+    }
+    if (password !== passwordConfirmation) {
+      return res.send("Password do not match");
+    }
+    const user = await usersDB.create({ email, password });
+    req.session.userId = user.id;
 
-  res.send("created");
-});
+    res.send("created");
+  }
+);
 
 router.get("/signout", (req, res) => {
   req.session = null;
