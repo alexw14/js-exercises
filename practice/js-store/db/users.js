b3d69a2c4b1c81f7cdda2a481e80111aba23df2b
Alex Wong <alexw14@gmail.com>
2020-04-09 22:41:19 -0700

    add hash and salt

--- practice/js-store/db/users.js
+++ practice/js-store/db/users.js
@@ -1,5 +1,8 @@
 const fs = require("fs");
 const crypto = require("crypto");
+const util = require("util");
+
+const scrypt = util.promisify(crypto.scrypt);
 
 class UsersDB {
   constructor(filename) {
@@ -23,11 +26,25 @@ class UsersDB {
   }
 
   async create(userData) {
+    // userData  === {email: "", password: ""}
     userData.id = this.generateRandomId();
+    const salt = crypto.randomBytes(8).toString("hex");
+    const hashed = await scrypt(userData.password, salt, 64);
     const records = await this.getAll();
-    records.push(userData);
+    const record = {
+      ...userData,
+      password: `${hashed.toString("hex")}.${salt}`
+    };
+    records.push(record);
     await this.writeAll(records);
-    return userData;
+    return record;
+  }
+
+  async comparePasswords(saved, supplied) {
+    // saved -> in database ("hashed.salt")
+    const [hashed, salt] = saved.split(".");
+    const hashedSupplied = await scrypt(supplied, salt, 64);
+    return hashed === hashedSupplied.toString("hex");
   }
 
   async writeAll(records) {
